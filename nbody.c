477a925c798e7789cd51c56901b6f26e6025d659
L-Bast <liambast@udel.edu>
2026-05-06 19:25:12 -0400

    fixed nbody

--- nbody.c
+++ nbody.c
@@ -8,16 +8,13 @@
 #include "compute.h"
 #include <cuda_runtime.h>
 
-// represents the objects in the system.  Global variables
+// represents the objects in the system. Global variables
 vector3 *hVel, *d_hVel;
 vector3 *hPos, *d_hPos;
 double *mass;
 double *d_mass;
 
-//initHostMemory: Create storage for numObjects entities in our system
-//Parameters: numObjects: number of objects to allocate
-//Returns: None
-//Side Effects: Allocates memory in the hVel, hPos, and mass global variables
+// initHostMemory
 void initHostMemory(int numObjects)
 {
 	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
@@ -25,10 +22,7 @@ void initHostMemory(int numObjects)
 	mass = (double *)malloc(sizeof(double) * numObjects);
 }
 
-//freeHostMemory: Free storage allocated by a previous call to initHostMemory
-//Parameters: None
-//Returns: None
-//Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
+// freeHostMemory
 void freeHostMemory()
 {
 	free(hVel);
@@ -36,11 +30,7 @@ void freeHostMemory()
 	free(mass);
 }
 
-//planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
-//				of our solar system (Sun+NUMPLANETS)
-//Parameters: None
-//Returns: None
-//Fills the first 8 entries of our system with an estimation of the sun plus our 8 planets.
+// planetFill
 void planetFill(){
 	int i,j;
 	double data[][7]={SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE};
@@ -53,14 +43,10 @@ void planetFill(){
 	}
 }
 
-//randomFill: FIll the rest of the objects in the system randomly starting at some entry in the list
-//Parameters: 	start: The index of the first open entry in our system (after planetFill).
-//				count: The number of random objects to put into our system
-//Returns: None
-//Side Effects: Fills count entries in our system starting at index start (0 based)
+// randomFill
 void randomFill(int start, int count)
 {
-	int i, j, c = start;
+	int i, j;
 	for (i = start; i < start + count; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -72,10 +58,7 @@ void randomFill(int start, int count)
 	}
 }
 
-//printSystem: Prints out the entire system to the supplied file
-//Parameters: 	handle: A handle to an open file with write access to prnt the data to
-//Returns: 		none
-//Side Effects: Modifies the file handle by writing to it.
+// printSystem
 void printSystem(FILE* handle){
 	int i,j;
 	for (i=0;i<NUMENTITIES;i++){
@@ -83,7 +66,7 @@ void printSystem(FILE* handle){
 		for (j=0;j<3;j++){
 			fprintf(handle,"%lf,",hPos[i][j]);
 		}
-		printf("),v=(");
+		fprintf(handle,"),v=(");
 		for (j=0;j<3;j++){
 			fprintf(handle,"%lf,",hVel[i][j]);
 		}
@@ -95,53 +78,46 @@ int main(int argc, char **argv)
 {
 	clock_t t0=clock();
 	int t_now;
-	//srand(time(NULL));
+
 	srand(1234);
+
 	initHostMemory(NUMENTITIES);
 	planetFill();
 	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
 
-	cudaMalloc(&d_hPos, sizeof(vector3) * NUMENTITIES);
-	cudaMalloc(&d_hVel, sizeof(vector3) * NUMENTITIES);
-	cudaMalloc(&d_mass, sizeof(double) * NUMENTITIES);
+	// GPU allocations
+	cudaMalloc((void**)&d_hPos, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**)&d_hVel, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**)&d_mass, sizeof(double) * NUMENTITIES);
 
-	cudaMemcpy(d_hPos, hPos,
-           sizeof(vector3) * NUMENTITIES,
-           cudaMemcpyHostToDevice);
+	// copy to GPU
+	cudaMemcpy(d_hPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
 
-	cudaMemcpy(d_hVel, hVel,
-           sizeof(vector3) * NUMENTITIES,
-           cudaMemcpyHostToDevice);
-
-	cudaMemcpy(d_mass, mass,
-           sizeof(double) * NUMENTITIES,
-           cudaMemcpyHostToDevice);
-	
-	//now we have a system.
-	#ifdef DEBUG
-	printSystem(stdout);
-	#endif
-	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
-		compute();
-	}
-
-	cudaMemcpy(hPos, d_hPos,
-           sizeof(vector3) * NUMENTITIES,
-           cudaMemcpyDeviceToHost);
-
-	cudaMemcpy(hVel, d_hVel,
-           sizeof(vector3) * NUMENTITIES,
-           cudaMemcpyDeviceToHost);
-	
-	clock_t t1=clock()-t0;
 #ifdef DEBUG
 	printSystem(stdout);
 #endif
+
+	for (t_now=0; t_now<DURATION; t_now+=INTERVAL){
+		compute();
+	}
+
+	// copy back
+	cudaMemcpy(hPos, d_hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
+
+	clock_t t1=clock()-t0;
+
+#ifdef DEBUG
+	printSystem(stdout);
+#endif
+
 	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
 
 	cudaFree(d_hPos);
 	cudaFree(d_hVel);
 	cudaFree(d_mass);
-	
+
 	freeHostMemory();
 }
